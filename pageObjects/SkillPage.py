0d2638099f1a94677a86b99c8bb4cc0d7897ef6f
TKTShubhamSingh <shubham.singh@Tktechnico.com>
2024-07-01 14:06:26 +0530

    Hirebase automation

--- pageObjects/SkillPage.py
+++ pageObjects/SkillPage.py
@@ -1,4 +1,8 @@
+import time
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class SkillPage:
@@ -6,13 +10,84 @@ class SkillPage:
         self.driver = driver
 
     def username(self, username):
-        self.driver.find_element(By.ID, "txtEmail").send_keys(username)
+        try:
+            self.driver.find_element(By.ID, "txtEmail").send_keys(username)
+        except Exception as e:
+            raise Exception(f"Error entering username: {e}")
 
     def password(self, password):
-        self.driver.find_element(By.ID, "txtPassword").send_keys(password)
+        try:
+            self.driver.find_element(By.ID, "txtPassword").send_keys(password)
+        except Exception as e:
+            raise Exception(f"Error entering password: {e}")
 
     def login(self):
-        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
+        try:
+            self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
+        except Exception as e:
+            raise Exception(f"Error clicking login button: {e}")
 
     def settings(self):
-        self.driver.find_element(By.CSS_SELECTOR, ".bg-transparent > img").click()
+        try:
+            self.driver.find_element(By.CSS_SELECTOR, ".bg-transparent > img").click()
+        except Exception as e:
+            raise Exception(f"Error clicking settings button: {e}")
+
+    def skills(self):
+        try:
+            self.driver.find_element(By.LINK_TEXT, "Skills").click()
+        except Exception as e:
+            raise Exception(f"Error clicking skills link: {e}")
+
+    def AddnewSkill(self, Skillname):
+        try:
+            self.driver.find_element(By.XPATH, "//button[@class='c-btn dark-btn']").click()
+        except Exception as e:
+            raise Exception(f"Error clicking Add new skill button: {e}")
+
+        time.sleep(2)
+
+        try:
+            self.driver.find_element(By.XPATH, "//input[@id='txtExperienceName']").send_keys(Skillname)
+        except Exception as e:
+            raise Exception(f"Error sending keys to the skill name input: {e}")
+
+    def fieldType(self):
+        element = self.driver.find_element(By.XPATH, "//select[@id='drpType']")
+        select = Select(element)
+        select.select_by_index(2)
+
+        time.sleep(2)
+
+    def options(self, Option1, Option2, Option3, Option4):
+        self.driver.find_element(By.ID, "txtExperienceOption-0").send_keys(Option1)
+        time.sleep(2)
+        button_locator = (By.XPATH, "//button[contains(text(),'Add Option')]")
+
+        try:
+            for _ in range(3):
+                button = WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable(button_locator)
+                )
+
+                button.click()
+
+        except Exception as e:
+            print("An error occurred:", e)
+
+        self.driver.find_element(By.ID, "txtExperienceOption-1").send_keys(Option2)
+        self.driver.find_element(By.ID, "txtExperienceOption-2").send_keys(Option3)
+        self.driver.find_element(By.ID, "txtExperienceOption-3").send_keys(Option4)
+
+    def Status(self):
+        element = self.driver.find_element(By.ID, "drpStatuses")
+        select = Select(element)
+        select.select_by_index(1)
+
+    def Skill_Group(self):
+        element = self.driver.find_element(By.ID, "drpSkillGroupId")
+        select = Select(element)
+        select.select_by_index(3)
+
+    def Save(self):
+        self.driver.find_element(By.XPATH, "//button[@class='c-btn dark-btn' and text()='Save']").click()
--- testCases/Skill.py
+++ testCases/Skill.py
@@ -1,10 +1,19 @@
 import logging
+import os
+import time
 import unittest
-
+import gspread
+import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pageObjects.SkillPage import SkillPage
+from HtmlTestRunner import HTMLTestRunner
 
 
-class Skill (unittest.TestCase):
+class Skill(unittest.TestCase):
     def setUp(self):
         logging.basicConfig(
             level=logging.INFO,
@@ -15,7 +24,76 @@ class Skill (unittest.TestCase):
             ]
         )
         self.logger = logging.getLogger(__name__)
-        self.logger.info("setting up the environment")
+        self.logger.info("Setting up the environment")
 
         scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-        creds = ServiceAccountCredentials.from_json_keyfile_name()
+        creds = ServiceAccountCredentials.from_json_keyfile_name("C:/pythonProject/Framework/TestData/hiredata.json",
+                                                                 scope)
+        client = gspread.authorize(creds)
+        spreadsheet = client.open('Leads')
+        self.sheet = spreadsheet.worksheet('Sheet1')
+
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+
+    def takeScreenShot(self, step_name):
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        screenshot_name = os.path.join('Screenshots', f"{step_name}_{timestamp}.png")
+        self.driver.save_screenshot(screenshot_name)
+        self.logger.info(f"ScreenShot taken: {screenshot_name}")
+
+    def test_Skill(self):
+        try:
+            self.driver.get("https://hirebaseproto.tktechnico.com/login")
+            df = pd.DataFrame(self.sheet.get_all_records())
+
+            self.Skills = SkillPage(self.driver)
+
+            for Outer_index, row in df.iterrows():
+                if Outer_index == 0:
+                    self.Skills.username(row['Username'])
+                    self.Skills.password(row['Password'])
+                    self.Skills.login()
+
+                    WebDriverWait(self.driver, 10).until(
+                        EC.visibility_of_element_located((By.CSS_SELECTOR, ".bg-transparent > img"))
+                    )
+                    self.takeScreenShot("Dashboard")
+                    act_title = self.driver.title
+                    self.logger.debug(f"Page title after login: {act_title}")
+                    assert act_title == "Hirebase", f"Expected title 'Hirebase', but got {act_title}'"
+                    self.logger.info("Login successful, title matched")
+
+                    self.Skills.settings()
+                    self.takeScreenShot("Settings")
+
+                    time.sleep(2)
+
+                    self.Skills.skills()
+
+                    self.Skills.AddnewSkill(row['Skillname'])
+
+                    time.sleep(2)
+                    self.Skills.fieldType()
+                    time.sleep(2)
+                    self.Skills.options(row['Option 1'], row['Option 2'], row['Option 3'], row['Option 4'])
+                    time.sleep(3)
+                    self.Skills.Status()
+                    time.sleep(1)
+                    self.Skills.Skill_Group()
+                    time.sleep(1)
+                    self.Skills.Save()
+                    time.sleep(1)
+
+        except Exception as e:
+            self.logger.error(f"An error occurred: {e}")
+            self.takeScreenShot("Error")
+            raise
+
+    def tearDown(self):
+        self.driver.quit()
+        self.logger.info("Test completed and browser closed")
+
+
+if __name__ == "__main__":
+    unittest.main(testRunner=HTMLTestRunner(output='Reports'))
