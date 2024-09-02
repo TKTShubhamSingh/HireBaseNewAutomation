fb3bf1bd3648fa835c33788247ad4eacfae74d0e
TKTShubhamSingh <shubham.singh@tktechnico.com>
2024-09-02 18:30:35 +0530

    Hirebase automation (02/09/2024)

--- pageObjects/EditCrmLeads.py
+++ pageObjects/EditCrmLeads.py
@@ -16,8 +16,7 @@ class EditCrm_Leads:
         self.wait = WebDriverWait(driver, 10)
 
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-        creds = ServiceAccountCredentials.from_json_keyfile_name("C:\\pythonProject\\Framework\\TestData"
-                                                                 "\\HireBase_data.json", scope)
+        creds = ServiceAccountCredentials.from_json_keyfile_name("C:\\pythonProject\\HireBase_data.json", scope)
         client = gspread.authorize(creds)
         spreadsheet = client.open('Leads')
         sheet = spreadsheet.worksheet('Sheet1')
